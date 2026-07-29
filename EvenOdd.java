59f20227915543700275c475d114eb8b08a49312
nandhini2007m <nandhinimaharajan111@gmail.com>
2026-07-29 20:41:49 +0530

    Correct syntax errors in EvenOdd.java
    Fix syntax errors and improve formatting in EvenOdd.java.

---
+++ EvenOdd.java
@@ -0,0 +1,20 @@
+import java.util.scanner
+  pulic class EvenOdd{
+    Public Static void main(String[]args)
+    {
+      Scsanner sc= new
+        Scanner(System.in);
+      System.out.print("Enter a number:);
+      Int num=sc.nextInt();
+      If(num % 2==0)
+        {
+        System.out.print(num+"is Even");
+      }
+        else
+        {
+          System.out.printIn(num+"is odd");
+        }
+      sc.close();
+    }
+  }
+          
